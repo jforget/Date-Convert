ecf912fd5cdfeeeef2db8003001f5a8eb3390fa2
Jean Forget <JFORGET@cpan.org>
2020-09-24 21:02:16 +0200

    Proper declaration of the copyright and license, including the GPL-required comments.

--- Convert.pm
+++ Convert.pm
@@ -1,3 +1,10 @@
+# -*- encoding: utf-8; indent-tabs-mode: nil -*-
+#
+#     Perl module to convert dates between Gregorian, Julian and Hebrew
+#     Copyright © 1997, 2000, 2020 Mordechai Abzug and Jean Forget
+#
+#     See the license in the embedded documentation below.
+#
 
 package Date::Convert;
 
@@ -830,9 +837,11 @@ perl(1), Date::DateCalc(3)
 
 Date::Convert 0.17 (pre-alpha)
 
-=head1 AUTHOR
+=head1 AUTHORS
 
-Mordechai T. Abzug <morty@umbc.edu>
+Module creator: Mordechai T. Abzug <morty at umbc dot edu>
+
+Unofficial co-maintainer: Jean Forget <JFORGET at cpan dot org>
 
 =head1 ACKNOWLEDGEMENTS AND FURTHER READING
 
@@ -865,12 +874,34 @@ reprinted just a few years ago.
 Steffen Beyer's Date::DateCalc showed me how to use MakeMaker and write POD
 documentation.  Of course, any error is my fault, not his!
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright 1997 by Mordechai T. Abzug
+Copyright  © 1997,  2000, 2020  Mordechai Abzug  and Jean  Forget. All
+rights reserved.
 
-=head1 LICENSE STUFF
+This  program  is  free  software. You  can  distribute,  modify,  and
+otherwise  mangle Date::Convert::French_Rev  under the  same terms  as
+Perl 5.16.3: GNU  Public License version 1 or later  and Perl Artistic
+License
 
-You can distribute, modify, and otherwise mangle Date::Convert under the
-same terms as perl.
+You can  find the text  of the licenses  in the F<LICENSE> file  or at
+L<https://dev.perl.org/licenses/artistic.html>
+and L<https://www.gnu.org/licenses/gpl-1.0.html>.
 
+Here is the summary of GPL:
+
+This program is  free software; you can redistribute  it and/or modify
+it under the  terms of the GNU General Public  License as published by
+the Free  Software Foundation; either  version 1, or (at  your option)
+any later version.
+
+This program  is distributed in the  hope that it will  be useful, but
+WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+General Public License for more details.
+
+You  should have received  a copy  of the  GNU General  Public License
+along with  this program; if not,  see <https://www.gnu.org/licenses/>
+or write to the Free Software Foundation, Inc., L<https://www.fsf.org>.
+
+=cut
